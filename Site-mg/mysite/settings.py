cae63582c1a02d245039af62f2d918d60d1eff9f
george <ujif12349@yandex.ru>
2021-12-12 14:49:02 +0300

    add import os

--- Site-mg/mysite/settings.py
+++ Site-mg/mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 #--------------------------------------------------------------
-#import os
+import os
 #import environ         #переменые окружения для баз данных
 #env = environ.Env()
 #--------------------------------------------------------------
